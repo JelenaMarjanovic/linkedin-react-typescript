e3c2fe18feb1c6dd24e639ba92b19fe36082f5f1
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-11-08 12:19:10 +0100

    Basic types

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const firstValue: string = 'React';
+  const secondValue: number = 2020;
+  const thirdValue: boolean = true;
+  const fourthValue: number[] = [3, 5, 7];
+  const fifthValue: Array<string> = ['React', 'Vue', 'Angular'];
+
+  const values: Array<any> = [firstValue, secondValue, thirdValue, fourthValue, fifthValue];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {values.map(value => (
+          <p>
+            This value {value} is of {typeof value} value!
+          </p>
+        ))}
       </header>
     </div>
   );
