3f9ba060f28703c7acb024ec192e374525b1682c
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-11-08 12:34:51 +0100

    Other compex types

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,32 @@ function App() {
   const thirdValue: boolean = true;
   const fourthValue: number[] = [3, 5, 7];
   const fifthValue: Array<string> = ['React', 'Vue', 'Angular'];
+  const tuple: [string, number] = ['React', 2020];
 
-  const values: Array<any> = [firstValue, secondValue, thirdValue, fourthValue, fifthValue];
+  const warning = (): void => {
+    console.log('Warning!');
+  }
+
+  const values: Array<any> = [
+    firstValue,
+    secondValue,
+    thirdValue,
+    fourthValue,
+    fifthValue,
+    tuple
+  ];
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        {values.map(value => (
-          <p>
+        {values.map((value, index) => (
+          <p key={index}>
             This value {value} is of {typeof value} value!
           </p>
         ))}
+        {warning()}
       </header>
     </div>
   );
